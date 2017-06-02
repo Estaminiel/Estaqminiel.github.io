11741f5900660a18eedde873384a7dd51f1e632a
Estaminiel <kusainova.liza@yandex.ru>
2017-06-02 10:46:28 +0300

    Файлы для сайта3

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Документ без названия</title>
+</head>
+
+<body>
+<p><strong> ЛЕТНИЕ РАЗВЛЕЧЕНИЯ
+</strong></p>
+<p>Лето-пора отдыха и равлечений. Рассмотрим наиболее популярные из них:</p>
+<p>АКВАПАРК</p>
+<p>Прокатись с ветерком и брызгами!</p>
+<p><img src="../../../Тюлькина Н/2 семестр/Ла. раб. 6/20-1mt.jpg" width="1280" height="843" /></p>
+<p>&nbsp;</p>
+<p>ДЕЛЬФИНАРИЙ</p>
+<p>Очаровательные и умные дельфины никого не оставят равнодушным!</p>
+<p><img src="../../../Тюлькина Н/2 семестр/Ла. раб. 6/delfinarij.jpg" width="1192" height="793" /></p>
+<p>&nbsp;</p>
+<p>ПЛЯЖ</p>
+<p>Ну какое же лето проходит без посещения пляжа?</p>
+<p><img src="../../../Тюлькина Н/2 семестр/Ла. раб. 6/196885_603x354.jpg" width="1188" height="606" /></p>
+</body>
+</html>
